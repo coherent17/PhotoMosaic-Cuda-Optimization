8a9d8f09c53252e59c67cbfc814d6ad23da86bb4
coherent17 <mnb51817@gmail.com>
2024-06-05 00:57:49 +0800

    parallel code with correct answer

--- inc/photo_mosaic_cuda.cuh
+++ inc/photo_mosaic_cuda.cuh
@@ -1,9 +1,13 @@
 #ifndef _PHOTO_MOSAIC_CUDA_H_
 #define _PHOTO_MOSAIC_CUDA_H_
 
+#include "rgb_image.h"
+#include <cuda_runtime.h>
+
 #define SUB_PIC_SIZE 32
 #define NUM_CANDIDATE_IMGS 10000
-#include "rgb_image.h"
+#define THREADS_PER_BLOCK 512
+#define IMAGE_DBL_MAX 300
 
 class Photo_Mosaic_Cuda{
 private:
@@ -11,6 +15,8 @@ private:
     RGBImage target_img;
     int num_candidate_imgs;
     vector<string> candidate_img_filenames;
+
+    // host memory
     double *r_avg_candidate;
     double *g_avg_candidate;
     double *b_avg_candidate;
@@ -18,6 +24,15 @@ private:
     double *g_avg_target_grid;
     double *b_avg_target_grid;
     int *min_idxs;
+
+    // device memory
+    double *d_r_avg_target_grid;
+    double *d_g_avg_target_grid;
+    double *d_b_avg_target_grid;
+    double *d_r_avg_candidate;
+    double *d_g_avg_candidate;
+    double *d_b_avg_candidate;
+    int *d_min_idxs;
 
 public:
     Photo_Mosaic_Cuda();
--- src/photo_mosaic_cuda.cu
+++ src/photo_mosaic_cuda.cu
@@ -1,5 +1,36 @@
 #include "photo_mosaic_cuda.cuh"
 
+__global__ void find_min_idx(
+    const double *r_avg_target_grid, 
+    const double *g_avg_target_grid,
+    const double *b_avg_target_grid, 
+    const double *r_avg_candidate,
+    const double *g_avg_candidate, 
+    const double *b_avg_candidate,
+    int num_candidate_imgs, 
+    int tile_width, 
+    int tile_height,
+    int *min_idxs
+) 
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < tile_width * tile_height) {
+        double min_diff = DBL_MAX;
+        for (int j = 0; j < num_candidate_imgs; ++j) {
+            double r_diff = r_avg_target_grid[i] - r_avg_candidate[j];
+            double g_diff = g_avg_target_grid[i] - g_avg_candidate[j];
+            double b_diff = b_avg_target_grid[i] - b_avg_candidate[j];
+            double diff = sqrt(r_diff * r_diff + g_diff * g_diff + b_diff * b_diff);
+
+            if (diff < min_diff) {
+                min_diff = diff;
+                min_idxs[i] = j;
+            }
+        }
+    }
+}
+
 Data_Loader Photo_Mosaic_Cuda::data_loader;
 
 Photo_Mosaic_Cuda::Photo_Mosaic_Cuda(){
@@ -14,6 +45,14 @@ Photo_Mosaic_Cuda::~Photo_Mosaic_Cuda(){
     free(g_avg_target_grid);
     free(b_avg_target_grid);
     free(min_idxs);
+
+    cudaFree(d_r_avg_target_grid);
+    cudaFree(d_g_avg_target_grid);
+    cudaFree(d_b_avg_target_grid);
+    cudaFree(d_r_avg_candidate);
+    cudaFree(d_g_avg_candidate);
+    cudaFree(d_b_avg_candidate);
+    cudaFree(d_min_idxs);
 }
 
 RGBImage *Photo_Mosaic_Cuda::Run(string targetImgPath, string candidateImgFolderPath){
@@ -40,6 +79,15 @@ RGBImage *Photo_Mosaic_Cuda::Run(string targetImgPath, string candidateImgFolder
     r_avg_candidate = (double *)malloc(sizeof(double) * num_candidate_imgs);
     g_avg_candidate = (double *)malloc(sizeof(double) * num_candidate_imgs);
     b_avg_candidate = (double *)malloc(sizeof(double) * num_candidate_imgs);
+
+    // allocate device memory
+    cudaMalloc((void **)&d_r_avg_target_grid, sizeof(double) * tile_width * tile_height);
+    cudaMalloc((void **)&d_g_avg_target_grid, sizeof(double) * tile_width * tile_height);
+    cudaMalloc((void **)&d_b_avg_target_grid, sizeof(double) * tile_width * tile_height);
+    cudaMalloc((void **)&d_r_avg_candidate, sizeof(double) * num_candidate_imgs);
+    cudaMalloc((void **)&d_g_avg_candidate, sizeof(double) * num_candidate_imgs);
+    cudaMalloc((void **)&d_b_avg_candidate, sizeof(double) * num_candidate_imgs);
+    cudaMalloc((void **)&d_min_idxs, sizeof(int) * tile_width * tile_height);
 
     // calculate the r_avg, g_avg, b_avg of the grid of the target image
     int target_grid_idx = 0;
@@ -71,21 +119,31 @@ RGBImage *Photo_Mosaic_Cuda::Run(string targetImgPath, string candidateImgFolder
         b_avg_candidate[i] = candidate_imgs[i].b_avg;
     }
 
-    for(int i = 0; i < tile_width * tile_height; i++){
-        double min_diff = DBL_MAX;
-        for(int j= 0; j < num_candidate_imgs; j++){
-            double r_diff = r_avg_target_grid[i] - r_avg_candidate[j];
-            double g_diff = g_avg_target_grid[i] - g_avg_candidate[j];
-            double b_diff = b_avg_target_grid[i] - b_avg_candidate[j];
-            double diff = sqrt(r_diff * r_diff + g_diff * g_diff + b_diff * b_diff);
-            if(diff < min_diff){
-                min_idxs[i] = j;
-                min_diff = diff;
-            }
-        }
-    }
+    // copy inputs to device
+    cudaMemcpy(d_r_avg_target_grid, r_avg_target_grid, sizeof(double) * tile_width * tile_height, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_g_avg_target_grid, g_avg_target_grid, sizeof(double) * tile_width * tile_height, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b_avg_target_grid, b_avg_target_grid, sizeof(double) * tile_width * tile_height, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_r_avg_candidate, r_avg_candidate, sizeof(double) * num_candidate_imgs, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_g_avg_candidate, g_avg_candidate, sizeof(double) * num_candidate_imgs, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b_avg_candidate, b_avg_candidate, sizeof(double) * num_candidate_imgs, cudaMemcpyHostToDevice);
 
-    
+    // for each grid image in target image, find the best fit candidate image from cifar10
+    find_min_idx<<<(tile_width * tile_height + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(
+        d_r_avg_target_grid,
+        d_g_avg_target_grid,
+        d_b_avg_target_grid,
+        d_r_avg_candidate,
+        d_g_avg_candidate,
+        d_b_avg_candidate,
+        num_candidate_imgs,
+        tile_width,
+        tile_height,
+        d_min_idxs
+    );
+
+    cudaDeviceSynchronize();
+    cudaMemcpy(min_idxs, d_min_idxs, sizeof(int) * tile_width * tile_height, cudaMemcpyDeviceToHost);
+
     int count = 0;
     for(int row = 0; row < target_img.get_h() - SUB_PIC_SIZE; row += SUB_PIC_SIZE){
         for(int col = 0; col < target_img.get_w() - SUB_PIC_SIZE; col += SUB_PIC_SIZE){
